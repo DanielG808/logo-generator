5feaf5b504d93f5308af25a845579427939af33f
Dan G <daniel.w.gray1994@gmail.com>
2023-06-13 06:57:22 -0400

    added a shape class and started a testing suite for the shape class

---
+++ tests/Shape.test.js
@@ -0,0 +1,9 @@
+const Shape = require("../lib/Shape");
+
+describe("Shape", () => {
+    it('should make a shape object', () => {
+        const shape = new Shape();
+
+        expect(shape instanceof Shape).toBe(true);
+    })
+})
